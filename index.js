99e271b82de87cc242f8312b8fbe752f86f39e10
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-02-04 07:52:03 +0300

    complited

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = 'HAYDEEE';
+
+/* const welcomeMessage = 'Welcome to Flatbook ' + currentUser
+ */
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+console.log(welcomeMessage)
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+console.log(excitedWelcomeMessage)
+
+const shortGreeting = `Welcome, ${currentUser.split("")[0]}!`;
+
+console.log(shortGreeting)
